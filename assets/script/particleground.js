571b595ce4ea44091e5001330aa66093c3f44ce4
niwaniwa <haniwadesuniwatoriman0202@gmail.com>
2015-10-16 09:28:08 +0900

    更新
    更新

--- assets/script/particleground.js
+++ assets/script/particleground.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
-    dotColor: '#929292',
-    lineColor: '#000000'
+    dotColor: '#FF9100',
+    lineColor: '#7c2b2b'
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';

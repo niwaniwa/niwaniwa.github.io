25ca49fc078f83c506cf145c38356d1d4e9263f2
niwaniwa <konya.kokekokko@gmail.com>
2017-01-11 01:04:27 +0900

    delete

--- assets/script/particleground.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  particleground(document.getElementById('particles'), {
-    dotColor: '#FF9100',
-    lineColor: '#7c2b2b'
-  });
-  var intro = document.getElementById('intro');
-  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
-}, false);
